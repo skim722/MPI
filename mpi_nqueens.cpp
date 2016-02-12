70ce8a609cf376a98e59512d26df7e1165099cec
Benson Ma <bma36@gatech.edu>
2016-02-13 04:29:20 +0900

    fixed the bug

--- mpi_nqueens.cpp
+++ mpi_nqueens.cpp
@@ -67,7 +67,7 @@ void master_solution_func(std::vector<unsigned int>& solution) {
     // First receive a message from any source.
     // By the symmetry of the code, the first message will either be WORKER_READY or WORK_DONE_MSG_N, and buffer will be size 1
     MPI_Recv(&buffer_size, 1, MPI_UNSIGNED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
-    printf("Process 0 got tag %d\n", stat.MPI_TAG);
+//    printf("Process 0 got tag %d\n", stat.MPI_TAG);
 
     // If the tag is WORK_DONE_MSG_N, then we must first receive the finished results before sending new work over
     // Otherwise, we continue (buffer_size won't be needed)
@@ -76,16 +76,13 @@ void master_solution_func(std::vector<unsigned int>& solution) {
 
         // Receive the actual solution set, from the SAME worker (stat.MPI_SOURCE)
         MPI_Recv(&finished_solutions[0], buffer_size, MPI_UNSIGNED, stat.MPI_SOURCE, WORK_DONE_MSG_BUFFER, MPI_COMM_WORLD, &stat);
-        printf("Process 0 got tag %d from process %d\n", stat.MPI_TAG, stat.MPI_SOURCE);
-printf("buffer size=%d\n", buffer_size);
-        //printf("                  !!sol 0 to 6 : %d %d %d | %d %d \n",finished_solutions[0],finished_solutions[1],finished_solutions[2],finished_solutions[3],finished_solutions[4],finished_solutions[5]);
+//        printf("Process 0 got tag %d from process %d\n", stat.MPI_TAG, stat.MPI_SOURCE);
         // Store the completed results that came from the worker
         MasterSolutionStore::add_solutions(finished_solutions);
-    
+
     }
     // Send new work to the SAME worker
-    printf("Process 0 sends tag %d to process %d\n", NEW_WORK_MSG, stat.MPI_SOURCE);
-  //  printf("                  sol 0 to 2 : %d %d %d | %d %d \n",solution[0],solution[1],solution[2],solution[3],solution[4],solution[5]);
+//    printf("Process 0 sends tag %d to process %d\n", NEW_WORK_MSG, stat.MPI_SOURCE);
     MPI_Send(&solution[0], solution.size(), MPI_UNSIGNED, stat.MPI_SOURCE, NEW_WORK_MSG, MPI_COMM_WORLD);
     //}
     // SK
@@ -167,6 +164,15 @@ struct WorkerSolutionStore {
     static void clear_solutions() {
         solutions().clear();
     }
+    static void print() {
+        for (int i=0; i < solutions().size(); ++i) {
+            printf("%d ", solutions()[i]);
+        } printf("\n");
+    }
+
+    static void clear() {
+        solutions().clear();
+    }
 };
 
 /**
@@ -221,24 +227,29 @@ void worker_main() {
         // Receive the buffer of size n and begin computation
         // MPI_Recv(void* data, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm communicator, MPI_Status* status)
         MPI_Recv(&partial_solution[0], n, MPI_UNSIGNED, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
-        printf("Process slave got tag %d\n", stat.MPI_TAG);
+//        printf("Process slave got tag %d\n", stat.MPI_TAG);
 
         // If we get the termination signal, exit the loop
         // http://stackoverflow.com/questions/10490983/mpi-slave-processes-hang-when-there-is-no-more-work
         if (stat.MPI_TAG == TERMINATION_MSG) {
-            printf("Process %d got the termination signal; exiting work loop.\n", rank);
+//            printf("Process %d got the termination signal; exiting work loop.\n", rank);
             break;
         }
+
+        // Clear the worker cache first, or repeat answers will be sent!!!
+        WorkerSolutionStore::clear();
 
         // Else, compute (saves solution to a long vector cache)
         nqueens_by_level(partial_solution, k, n, &worker_solution_func);
         std::vector<unsigned int> &vec_buffer = WorkerSolutionStore::solutions();
         unsigned int buffer_size = vec_buffer.size();
 
+        // WorkerSolutionStore::print();
+
         // Send the solutions buffer size (m solutions means the solutions vector is of size m x n), so the recipient knows how much to receive
-        printf("Process %d sends tag %d\n", rank, WORK_DONE_MSG_N);
+//        printf("Process %d sends tag %d\n", rank, WORK_DONE_MSG_N);
         MPI_Send(&buffer_size, 1, MPI_UNSIGNED, 0, WORK_DONE_MSG_N, MPI_COMM_WORLD);
-        printf("Process %d sends tag %d\n", rank, WORK_DONE_MSG_BUFFER);
+//        printf("Process %d sends tag %d\n", rank, WORK_DONE_MSG_BUFFER);
         MPI_Send(&vec_buffer[0], buffer_size, MPI_UNSIGNED, 0, WORK_DONE_MSG_BUFFER, MPI_COMM_WORLD);
         // MPI_Send(&buffer_size, 1, MPI_UNSIGNED, 0, 528492, MPI_COMM_WORLD);
         // MPI_Send(&vec_buffer[0], buffer_size, MPI_UNSIGNED, 0, 528493, MPI_COMM_WORLD); //ask more work tag: 528492 528493
